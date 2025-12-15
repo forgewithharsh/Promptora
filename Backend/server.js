40d30b2f3d458d8cc52ebf259d980f83836dc478
Harsh Guleria <guleriah59@gmail.com>
2025-12-15 16:47:36 +0530

    server.js code runs

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,13 @@
 import OpenAI from "openai";
+import "dotenv/config";
 
 const client = new OpenAI({
-  apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const response = await client.responses.create({
-  model: "gpt-4o",
-  instructions: "You are a coding assistant that talks like a pirate",
-  input: "Are semicolons optional in JavaScript?",
+  model: "gpt-4o-mini",
+  input: "Why do programmers prefer dark mode?",
 });
+
+console.log(response.output_text);
