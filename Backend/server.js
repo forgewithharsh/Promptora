0947877c53d67f8c2b8f87570d6de2c5aa6c009e
Harsh Guleria <guleriah59@gmail.com>
2025-12-15 17:35:07 +0530

    Refactor server.js to implement OpenAI API integration and set up Express server

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,43 @@
-import OpenAI from "openai";
+import express, { response } from "express";
 import "dotenv/config";
+import cors from "cors";
 
-const client = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
 
-const response = await client.responses.create({
-  model: "gpt-4o-mini",
-  input: "Why do programmers prefer dark mode?",
-});
+app.post("/test", async (req, res) => {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": `application/json`,
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          role: "user",
+          content: req.body.message,
+        },
+      ],
+    }),
+  };
 
-console.log(response.output_text);
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      options
+    );
+    const data = await response.json();
+    res.send(data.choices[0].message.content);
+  } catch (error) {
+    console.log(error);
+  }
+});
