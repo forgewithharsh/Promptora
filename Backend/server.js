9f5cc1664643585ec557ab12cfb535dabf4e6027
Harsh Guleria <guleriah59@gmail.com>
2025-12-17 20:20:05 +0530

    connected with mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express, { response } from "express";
 import "dotenv/config";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 8080;
@@ -10,34 +11,44 @@ app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
+  connectDB();
 });
 
-app.post("/test", async (req, res) => {
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": `application/json`,
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-    },
-    body: JSON.stringify({
-      model: "gpt-4o-mini",
-      messages: [
-        {
-          role: "user",
-          content: req.body.message,
-        },
-      ],
-    }),
-  };
-
+const connectDB = async () => {
   try {
-    const response = await fetch(
-      "https://api.openai.com/v1/chat/completions",
-      options
-    );
-    const data = await response.json();
-    res.send(data.choices[0].message.content);
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Connected with Database!");
   } catch (error) {
-    console.log(error);
+    console.log("Failed to connect with DB", error);
   }
-});
+};
+
+// app.post("/test", async (req, res) => {
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": `application/json`,
+//       Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+//     },
+//     body: JSON.stringify({
+//       model: "gpt-4o-mini",
+//       messages: [
+//         {
+//           role: "user",
+//           content: req.body.message,
+//         },
+//       ],
+//     }),
+//   };
+
+//   try {
+//     const response = await fetch(
+//       "https://api.openai.com/v1/chat/completions",
+//       options
+//     );
+//     const data = await response.json();
+//     res.send(data.choices[0].message.content);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
