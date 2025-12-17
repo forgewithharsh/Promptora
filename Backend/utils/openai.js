f40cb072611d45c95c57056a1bba7bb7bf374bde
Harsh Guleria <guleriah59@gmail.com>
2025-12-17 18:39:06 +0530

    basiuc setup of utils

---
+++ Backend/utils/openai.js
@@ -0,0 +1,35 @@
+import "dotenv/config";
+
+const getOpenAiApiResponse = async (message) => {
+  app.post("/test", async (req, res) => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": `application/json`,
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-4o-mini",
+        messages: [
+          {
+            role: "user",
+            content: message,
+          },
+        ],
+      }),
+    };
+
+    try {
+      const response = await fetch(
+        "https://api.openai.com/v1/chat/completions",
+        options
+      );
+      const data = await response.json();
+      return data.choices[0].message.content;
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
+export default getOpenAiApiResponse;
