4657252352061bba35aa1d9f96e71d56ab83208e
Harsh Guleria <guleriah59@gmail.com>
2025-12-18 02:50:55 +0530

    also setup: allmessages and delete route in chat.js

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router.post("/test", async (req, res) => {
   try {
     const thread = new Thread({
-      threadId: "xyz",
-      title: "testing New Thread",
+      threadId: "abc",
+      title: "Testing New Thread2",
     });
 
     const response = await thread.save();
@@ -20,15 +20,47 @@ router.post("/test", async (req, res) => {
 });
 
 // Get all threads
-router.get("/thread",async(req,res) => {
-    try {
-        const threads = await Thread.find({}).sort({updatedAt: -1});
-        //descending order of updatedAt...most recent data on top
-        res.json(threads)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ error: "Failed to fetch threads" });
+router.get("/thread", async (req, res) => {
+  try {
+    const threads = await Thread.find({}).sort({ updatedAt: -1 });
+    //descending order of updatedAt...most recent data on top
+    res.json(threads);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to fetch threads" });
+  }
+});
+
+router.get("/thread/:threadId", async (req, res) => {
+  const { threadId } = req.params;
+  try {
+    const thread = await Thread.findOne({ threadId });
+
+    if (!thread) {
+      res.status(404).json({ error: "Thread not found" });
     }
-})
+
+    res.json(thread.messages);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to fetch chat" });
+  }
+});
+
+router.delete("/thread/:threadId", async (req, res) => {
+  const { threadId } = req.params;
+  try {
+    const deleteThread = await Thread.findOneAndDelete({ threadId });
+
+    if (!deleteThread) {
+      res.status(404).json({ error: "Thread not found" });
+    }
+
+    res.status(200).json({ success: "Thread deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to delete thread" });
+  }
+});
 
 export default router;
