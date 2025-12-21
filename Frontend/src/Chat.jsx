64b26328a11aa3260eb40f709267ed3cecd75a0a
Harsh Guleria <guleriah59@gmail.com>
2025-12-21 14:23:05 +0530

    add typing effect in promptora chat

--- Frontend/src/Chat.jsx
+++ Frontend/src/Chat.jsx
@@ -1,17 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./Chat.css";
 import { MyContext } from "./MyContext.jsx";
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
-import "highlight.js/styles/github-dark.css"
+import "highlight.js/styles/github-dark.css";
 
 function Chat() {
-  const { newChat, prevChats } = useContext(MyContext);
+  const { newChat, prevChats, reply } = useContext(MyContext);
+  const [latestReply, setLatestReply] = useState(null);
+
+  useEffect(() => {
+    if (!prevChats?.length) return;
+
+    const content = reply.split(" "); //individual words
+
+    let idx = 0;
+    const interval = setInterval(() => {
+      setLatestReply(content.slice(0, idx + 1).join(" "));
+
+      idx++;
+      if (idx >= content.length) clearInterval(interval);
+    }, 40);
+
+    return () => clearInterval(interval);
+  }, [prevChats, reply]);
+
   return (
     <>
       {newChat && <h1>Start a New Chat!</h1>}
       <div className="chats">
-        {prevChats?.map((chat, idx) => (
+        {prevChats?.slice(0, -1).map((chat, idx) => (
           <div
             className={chat.role === "user" ? "userDiv" : "gptDiv"}
             key={idx}
@@ -19,10 +37,22 @@ function Chat() {
             {chat.role === "user" ? (
               <p className="userMessage">{chat.content}</p>
             ) : (
-              <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{chat.content}</ReactMarkdown>
+              <ReactMarkdown rehypePlugins={[rehypeHighlight]}>
+                {chat.content}
+              </ReactMarkdown>
             )}
           </div>
         ))}
+
+        {
+          prevChats.length > 0 && latestReply !== null &&
+          <div className="gptDiv" key={"typing"}>
+
+            <ReactMarkdown rehypePlugins={[rehypeHighlight]}>
+                {latestReply}
+              </ReactMarkdown>
+          </div>
+        }
       </div>
     </>
   );
